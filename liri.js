8b8c83f465f939f18c35244a76af017f3c13eeea
mjtedder <mjtedder@gmail.com>
2018-05-16 23:09:06 -0400

    corrected files necessary to run app

--- liri.js
+++ liri.js
@@ -1,8 +1,34 @@
-require("dotenv").config(); 
-var keys = require('./keys.js');
+require("dotenv").config();
+
+var keys = require('./keys');
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var moment = require('moment');
+var request = require('request');
+var fs = require('fs');
+
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var twitter = new Twitter(keys.twitter);
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+function log_Data(str = '', type = '', time = moment().format('MM/DD/YYYY hh:mm:ss')){
+
+    if (type) {
+        fs.appendFileSync('./log.txt', type + " request made at: " + time + '\n');
+    }
+    if (str) {
+        console.log(str);
+    
+    fs.appendFileSync('./log.txt', str);
+    }
+}
+
+function get_Tweets() {
+    log_Data('',"Tweet")
+
+    twitter.get('statuses/user_timeline', function(err, response) {})
 
 client.stream('statuses/filter', {track: 'twitter'}, function(stream) {
     stream.on('data', function(tweet) {
@@ -12,4 +38,4 @@ client.stream('statuses/filter', {track: 'twitter'}, function(stream) {
     stream.on('error', function(error) {
         console.log(error);
     });
-});
+})
