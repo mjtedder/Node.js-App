60c2e6d1e1c52095197c63851a6eab9e3abf41b1
mjtedder <mjtedder@gmail.com>
2019-02-15 08:36:29 -0700

    Application will take in multiple word requests
    Add two variables, one to slice the input of various medias, and one to
    join those multiple words in order to execute multiple word searches.
    
    This is an important addition because most songs/movies are more than
    one word.

--- liri2.js
+++ liri2.js
@@ -1,5 +1,5 @@
 // REQUIRED DEPENDENCIES =============================================
-require ('dotenv').config()
+require('dotenv').config()
 var moment = require('moment')
 var axios = require('axios')
 var fs = require('fs')
@@ -12,16 +12,29 @@ var spotify = new Spotify(keys.spotify)
 
 // SAVING USER INPUT TO VARIABLES ====================================
 var command = process.argv[2]
-var input = process.argv[3]
+var multipleWords = process.argv.slice(3)
+var input = multipleWords.join(' ')
 
 
 // SWITCH-CASE STATEMENT FOR LIRI COMMANDS ===========================
 function startApp(command, input) {
     switch (command) {
         case 'concert-this':
-        console.log(keys)
-        console.log(input)
-        break;
+            console.log(input)
+            //bandsThis(input)
+            break;
+        case 'spotify-this-song':
+            spotifyThis(input)
+            break;
+        case 'movie-this':
+            movieThis(input)
+            break;
+        case 'do-what-it-says':
+            doThis();
+            break;
+        default:
+            console.log("I'm sorry, LIRI doesn't recognize that command.")
+
     }
 }
 
